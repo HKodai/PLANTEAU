6f6647ee2e5787e33b3c584d4637a8af63ea710c
Kodai <guangtianhuangda@gmail.com>
2025-01-21 15:37:58 +0900

    説明を追加

--- plant.py
+++ plant.py
@@ -1,26 +1,28 @@
 from abc import ABC, abstractmethod
 
 
-class Plant(ABC):
+class Plant(ABC): # 植物のクラス
     def __init__(self, height):
-        self.height = height
+        self.height = height # 植物の高さ(単位:m)
 
     @abstractmethod
     def grow(self, sunlight):
+        # sunlight: 1日の日照時間(単位:時間)
+        # heightを更新する
         pass
 
 
-class DeciduousTree(Plant):
+class DeciduousTree(Plant): # 落葉樹のクラス
     @abstractmethod
-    def color(self, month):
+    def color(self, month): # 月によって葉の色を返すメソッド
         pass
 
     @abstractmethod
-    def fall(self, month):
+    def fall(self, month): # 月によって葉が落ちる量を返すメソッド
         pass
 
 
-class Gingko(DeciduousTree):
+class Gingko(DeciduousTree): # イチョウのクラス
     def __init__(self, height):
         super().__init__(height)
 
@@ -36,3 +38,6 @@ class Gingko(DeciduousTree):
         if month == 12:
             return 1700
         return 0
+    
+# 植物のクラスを作る場合は、Plantクラスを継承して、growメソッドを実装する必要がある。
+# また、落葉樹のクラスを作る場合は、DeciduousTreeクラスを継承して、colorメソッドとfallメソッドを実装する必要がある。
